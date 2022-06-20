ecfa84667042df7edc5579c129ba07bc8914c375
frizqui615 <104373038+frizqui615@users.noreply.github.com>
2022-06-20 12:00:40 +0200

    Add files via upload

---
+++ terraform.tfvars
@@ -0,0 +1,5 @@
+var.aws_access_key = "XXXXXXXXXXXXXXXXXXXX"
+var.aws_secret_key = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+#aws_key_path = "~/.ssh/bosh.pem"
+#aws_key_name = "bosh"
+var.aws_region = "eu-west-1"
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "var.aws_region" {
+     type = string
+     default = "eu-west-1"
+}
+variable "var.aws_access_key" {
+     type = string
+     default = "Access key to AWS console"
+}
+variable "var.aws_secret_key" {
+     type = string
+     default = "Secret key to AWS console"
+}
