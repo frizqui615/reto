588809ee177dfbcd16965b46028198954a04cf01
frizqui615 <104373038+frizqui615@users.noreply.github.com>
2022-06-21 11:25:47 +0200

    corrijo variable providers

--- providers.tf
+++ providers.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region  = var_aws_region
+  region  = "eu-west-1"
 
   # You can use access keys
-  access_key = var_aws_access_key
-  secret_key = var_aws_secret_key
+  # access_key = var_aws_access_key
+  # secret_key = var_aws_secret_key
 
   # Or specify an aws profile, instead.
   # profile = "<aws profile>"
