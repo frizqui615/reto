862ca25c51dc5559e894b93dd02741baf79e13ae
frizqui615 <104373038+frizqui615@users.noreply.github.com>
2022-06-21 21:11:49 +0200

    cambio providers

--- providers.tf
+++ providers.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region  = "eu-west-1"
 
   # You can use access keys
-   access_key = var_aws_access_key
-   secret_key = var_aws_secret_key
+  # access_key = var_aws_access_key
+  # secret_key = var_aws_secret_key
 
   # Or specify an aws profile, instead.
   # profile = "<aws profile>"
