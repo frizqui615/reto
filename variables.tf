11e30a309707db6b75943a2f8453866a475beba4
frizqui615 <104373038+frizqui615@users.noreply.github.com>
2022-06-21 08:29:40 +0200

    Add files via upload

--- variables.tf
+++ variables.tf
@@ -1,12 +1,12 @@
-variable "var_aws_region" {
+variable "var.aws_region" {
      type = string
      default = "eu-west-1"
 }
-variable "var_aws_access_key" {
+variable "var.aws_access_key" {
      type = string
      default = "Access key to AWS console"
 }
-variable "var_aws_secret_key" {
+variable "var.aws_secret_key" {
      type = string
      default = "Secret key to AWS console"
 }
